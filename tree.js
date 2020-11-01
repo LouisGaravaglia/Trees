e9caccf17c953c48b677a79a4b7bad9e344f99c3
Louis Garavaglia <louis.garavaglia@gmail.com>
2020-11-01 12:59:15 -0800

    finished numGreater method

--- tree.js
+++ tree.js
@@ -39,14 +39,50 @@ class Tree {
   /** countEvens(): count all of the nodes in the tree with even values. */
 
   countEvens() {
+ if (!this.root) return 0;
 
+    let count = this.root.val % 2 === 0 ? 1 : 0;
+
+    function countEvensHelper(node) {
+      // go through all the children for a Node
+      for (let child of node.children) {
+        // increment count by 1 if child value is even
+        if(child.val % 2 === 0) count++;
+        // if it has any children
+        if (child.children.length > 0) {
+          // recurse with the child as the root
+          countEvensHelper(child);
+        }
+      }
+    }
+
+    countEvensHelper(this.root);
+    return count;
   }
 
   /** numGreater(lowerBound): return a count of the number of nodes
    * whose value is greater than lowerBound. */
 
   numGreater(lowerBound) {
+    if (!this.root) return 0;
+    
+    let count = this.root.val > lowerBound ? 1 : 0;
 
+    function numGreaterHelper(node) {
+      // go through all the children for a Node
+      for (let child of node.children) {
+        // increment count by 1 if child value is even
+        if(child.val > lowerBound) count++;
+        // if it has any children
+        if (child.children.length > 0) {
+          // recurse with the child as the root
+          numGreaterHelper(child);
+        }
+      }
+    }
+
+    numGreaterHelper(this.root);
+    return count;
   }
 }
 
